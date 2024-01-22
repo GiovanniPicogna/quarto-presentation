6ea0caa00f6deb79c317b37df6a6be37741bd2d5
GiovanniPicogna <picogna@usm.lmu.de>
2024-01-22 13:37:50 +0100

    first commit

---
+++ 5-teaching/constants.py
@@ -0,0 +1,27 @@
+from astropy import units as u
+G = 6.6726e-11 * u.m**3 / u.kg / u.s**2
+c = 2.99792458e8 * u.m / u.s
+h = 6.62607015e-34 * u.J * u.s
+k_B = 1.380649e-23 * u.J / u.K
+sigma = 5.670374419e-8 * u.W / u.m**2 / u.K**4
+pc = 3.09e16 * u.m
+au = 149.5979e9 * u.m
+T_Sun = 5772. * u.K
+T_Earth = 290. * u.K
+T_Jupiter = 160. * u.K
+R_Sun = 6.9570e8 * u.m
+R_Earth = 6.3781e6 * u.m
+R_Jupiter = 7.1492e7 * u.m
+R_Neptune = 2.47e7 * u.m
+R_Venus = 6.05e6 * u.m
+a_Jupiter = 5.2 * au
+a_Mercury = 0.38 * au
+a_Venus = 0.72 * au
+a_Neptune = 30.1 * au
+M_Sun = 1.989e30 * u.kg
+M_Earth = 5.972e24 * u.kg
+M_Jupiter = 1.899e27 * u.kg
+M_Mercury = 3.30e23 * u.kg
+albedo_vis_Jupiter = 0.34
+albedo_vis_Earth = 0.31
+M_vis_Sun = 4.83
